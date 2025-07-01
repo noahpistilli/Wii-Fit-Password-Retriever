20530312200c871a709d4af8187f8971ae817753
Larsen Vallecillo <larsen@Weborican.com>
2025-07-01 15:47:42 -0500

    Add Korea and Taiwan save paths

--- save.h
+++ save.h
@@ -16,7 +16,9 @@ private:
     std::map<std::string_view, std::string_view> SAVE_PATHS = {
         {"Japan", "/title/00010004/5246504a/data/FitPlus0.dat"},
         {"Europe", "/title/00010004/52465050/data/FitPlus0.dat"},
-        {"NTSC-U", "/title/00010004/52465045/data/FitPlus0.dat"},
+        {"USA", "/title/00010004/52465045/data/FitPlus0.dat"},
+        {"Korea", "/title/00010004/5246504b/data/FitPlus0.dat"},
+        {"Taiwan", "/title/00010004/52465057/data/FitPlus0.dat"},
     };
 
     void GetSaves();
